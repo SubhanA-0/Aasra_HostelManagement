a8d6c238d565569c2d595b401a8026a1e1639ae5
SubhanA-0 <subhanogahmed06@gmail.com>
2026-05-04 12:55:51 +0500

    Fix: Express 5 wildcard routing issue

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use('/api/notices', noticesRoutes);
 
 // Serve frontend in production
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
